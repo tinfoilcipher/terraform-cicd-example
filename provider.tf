3bee7c168edb09ea3d6264c3aa47b18a3682db9a
tinfoilcipher <39910469+tinfoilcipher@users.noreply.github.com>
2020-05-08 21:21:28 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,3 +3,9 @@ provider "aws" {
     access_key      = "${data.vault_generic_secret.kv-aws-terraform-iam.data["access_key"]}"
     secret_key      = "${data.vault_generic_secret.kv-aws-terraform-iam.data["secret_key"]}"
 }
+
+provider "vault" {
+    address         = "${var.vault_url}"
+    skip_tls_verify = false
+    token           = "${var.vault_token}"
+}
