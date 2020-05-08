f0f47bfdab26e8768cc1f3398aec19faa8e175b3
tinfoilcipher <39910469+tinfoilcipher@users.noreply.github.com>
2020-05-08 18:28:41 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region          = "eu-west-2"
+    region          = "${var.region}"
     access_key      = "${data.vault_generic_secret.kv-aws-terraform-iam.data["access_key"]}"
     secret_key      = "${data.vault_generic_secret.kv-aws-terraform-iam.data["secret_key"]}"
 }
